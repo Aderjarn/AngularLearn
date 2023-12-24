a2e37f7862c9dc3dd0e97f1fec5c8f8712eac391
Fabián Avilés <feacfabian@gmail.com>
2023-12-24 17:11:30 -0600

    Mod2 Assignment

---
+++ mod2-Assignment/assignment2-starter-code/app.js
@@ -0,0 +1,58 @@
+(function () {
+    'use strict';
+    
+    angular.module('ShoppingListCheckOff', [])
+    .controller('ToBuyController', ToBuyController)
+    .controller('AlreadyBoughtController', AlreadyBoughtController)
+    .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+    ToBuyController.$inject = ['ShoppingListCheckOffService'];
+    function ToBuyController(ShoppingListCheckOffService){
+        var toBuyCtrl = this;
+        toBuyCtrl.toBuyList = ShoppingListCheckOffService.getToBuyList();
+
+        toBuyCtrl.buyItem = function (index) {
+            ShoppingListCheckOffService.buyItem(index);
+        };
+    }
+
+    AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+    function AlreadyBoughtController(ShoppingListCheckOffService){
+        var boughCtrl = this;
+        boughCtrl.alreadyBoughtList = ShoppingListCheckOffService.getAlreadyBoughtList();
+    }
+
+    function ShoppingListCheckOffService() {
+        var service = this;
+
+        var toBuyList = [
+            { name: 'Cookies', quantity: 10 },
+            { name: 'Milk', quantity: 2 },
+            { name: 'Bread', quantity: 1 },
+            { name: 'Eggs', quantity: 12 },
+            { name: 'Bananas', quantity: 6 }
+        ];
+
+        var alreadyBoughtList = [];
+
+        service.getToBuyList = function () {
+            return toBuyList;
+        };
+
+        service.getAlreadyBoughtList = function () {
+            return alreadyBoughtList;
+        };
+
+        service.buyItem = function (index) {
+            var boughtItem = toBuyList.splice(index, 1)[0];
+            alreadyBoughtList.push(boughtItem);
+
+            if (toBuyList.length === 0) {
+                service.everythingBoughtMessage = "Everything is bought!";
+            }
+            if (alreadyBoughtList.length === 0) {
+                service.nothingBoughtMessage = "Nothing bought yet";
+            }
+        };
+    }
+})();
